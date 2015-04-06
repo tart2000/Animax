14d99d7d634c7bd2759ce57cd926563df410b448
tart2000 <tart2000design@gmail.com>
2015-04-05 21:57:41 -0400

    getting better

--- site/snippets/nextedition.php
+++ site/snippets/nextedition.php
@@ -1,4 +1,4 @@
-<div id="next-edition" class="pt pb">
+<div id="next-edition" class="lpt pb">
     <div class="container mb">
         <div class="row">
         <?php $today=date('c'); ?>
@@ -8,7 +8,7 @@
                 <h1><?php echo $nextEdition->title(); ?></h1>
             </div>
             <div class="col-md-4 col-xs-12">
-                <h2 class="right"><?php echo $nextEdition->date('j', 'startDate') ?> - <?php echo $nextEdition->date('j M Y', 'endDate') ?></h2>
+                <h2 class="right"><i class="fa fa-calendar bmr"></i><?php echo $nextEdition->date('j', 'startDate') ?> - <?php echo $nextEdition->date('j M Y', 'endDate') ?></h2>
                 <h2 class="right"><a href="<?php echo $nextEdition->locationLink() ?>"><?php echo $nextEdition->location() ?></a></h2>
                 <?php if ($nextEdition->inscriptions() != "") : ?>
                     <a href="<?php echo $nextEdition->inscriptions() ?>" class="btn btn-theme btn-lg right">Inscriptions</a>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,29 +1,29 @@
 <?php snippet('header') ?>
 
+<?php snippet('head') ?>
 
+<a href="#next-edition"><div id="godown" class="fa fa-arrow-down"></div></a>
 
-    <?php snippet('head') ?>
+<?php snippet('nextedition') ?>
 
-    <?php snippet('nextedition') ?>
-
-    <div class="container mt">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2 center">
-                <h1>Animax, c’est LE laboratoire collaboratif qui innove en management !</h1>
-            </div>
-            <div class="col-md-5 col-md-offset-1 intro">
-                <h3>Pourquoi ?</h3>
-                <?php echo $page->pourquoi()->kirbytext() ?>
-            </div>
-            <div class="col-md-5 intro">
-                <h3>Comment ?</h3>
-                <p><?php echo $page->comment()->kirbytext() ?></p>
-            </div>
-            <div class="col-md-10 col-md-offset-1 intro">
-                <p><?php echo $page->text()->kirbytext() ?></p>
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 center">
+            <h1>Animax, c’est LE laboratoire collaboratif qui innove en management !</h1>
+        </div>
+        <div class="col-md-5 col-md-offset-1 intro">
+            <h3>Pourquoi ?</h3>
+            <?php echo $page->pourquoi()->kirbytext() ?>
+        </div>
+        <div class="col-md-5 intro">
+            <h3>Comment ?</h3>
+            <p><?php echo $page->comment()->kirbytext() ?></p>
+        </div>
+        <div class="col-md-10 col-md-offset-1 intro">
+            <p><?php echo $page->text()->kirbytext() ?></p>
         </div>
     </div>
+</div>
 
 
 
